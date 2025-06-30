6d907073bdf75a91a470071a9e38c7b334f397cb
Shivendra <shivharsh44@gmail.com>
2025-07-01 00:11:38 +0530

    ``linear`` transform function added

---
+++ axon/csrc/linalg/transform.h
@@ -0,0 +1,12 @@
+#ifndef __TRANSFORM__H__
+#define __TRANSFORM__H__
+
+#include "../core/core/core.h"
+
+extern "C" {
+  Array* linear_1d_array(Array* weight, Array* input, Array* bias);
+  Array* linear_2d_array(Array* weight, Array* input, Array* bias);
+  Array* linear_transform_array(Array* weights, Array* input, Array* bias);
+}
+
+#endif  //!__TRANSFORM__H__
