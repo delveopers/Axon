19a0d43287a520ff2ee62ba477fb2f82af27d825
Shivendra <shivharsh44@gmail.com>
2025-07-13 14:00:21 +0530

    fixed import issue & version update

--- axon/__init__.py
+++ axon/__init__.py
@@ -1,5 +1,6 @@
 from ._core import array, int8, int16, int32, int64, long, float32, float64, double, uint8, uint16, uint32, uint64, boolean
 from ._utils import randn, randint, uniform, linspace, fill, zeros, zeros_like, ones, ones_like
-from .linalg.transform import linear
-from .linalg.decompose import det, eign, eignh, eignhv, eignv
-from .linalg.vector import cross, dot, inner, outer, dot_mv
+from . import linalg
+
+__version__ = '0.0.2'
+__author__ = 'Shivendra S'
