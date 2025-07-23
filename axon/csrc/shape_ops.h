280cbb34e1067bbf83cd2d60c19224c1417ede04
Shivendra <shivharsh44@gmail.com>
2025-07-23 18:13:47 +0530

    added comparision ops

--- axon/csrc/shape_ops.h
+++ axon/csrc/shape_ops.h
@@ -8,6 +8,17 @@ extern "C" {
   // shaping ops
   Array* transpose_array(Array* a);
   Array* equal_array(Array* a, Array* b);
+  Array* equal_scalar(Array* a, float b);
+  Array* not_equal_array(Array* a, Array* b);
+  Array* not_equal_scalar(Array* a, float b);
+  Array* greater_array(Array* a, Array* b);
+  Array* greater_scalar(Array* a, float b);
+  Array* greater_equal_array(Array* a, Array* b);
+  Array* greater_equal_scalar(Array* a, float b);
+  Array* smaller_array(Array* a, Array* b);
+  Array* smaller_scalar(Array* a, float b);
+  Array* smaller_equal_array(Array* a, Array* b);
+  Array* smaller_equal_scalar(Array* a, float b);
   Array* reshape_array(Array* a, int* new_shape, int new_ndim);
   Array* squeeze_array(Array* a, int axis);
   Array* expand_dims_array(Array* a, int axis);
